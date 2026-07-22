e5143a460d6ec3f910e2e2d09c6ac6305fa38132
Justine Danielle L. Betito <justine_danielle_betito@dlsl.edu.ph>
2026-07-23 03:05:45 +0800

    gt4 error fixed in app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { fetchSampleUsers, fetchSampleUsersPromise } from './api.js';
 console.log(formatDate(new Date("2026-07-22")));
 console.log(validateTask());
 console.log(validateTask({ title: "Finish GT3", dueDate: "2026-07-22" }));
-console.log(mergeTaskUpdate({ title: "Old" }, { title: "New" }));\
+console.log(mergeTaskUpdate({ title: "Old" }, { title: "New" }));
 
 //gt4 updating from here
 
